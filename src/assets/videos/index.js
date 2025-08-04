7107f2c66acb359c1ed934de1eda9afd6117d149
Ofarouq310 <ofarouq310@gmail.com>
2025-08-04 20:40:21 +0300

    Combine videos imports in one file

---
+++ src/assets/videos/index.js
@@ -0,0 +1,17 @@
+import legendsOpening from './legends-opening.mp4';
+import kakarotOpening from './kakarot-opening.mp4';
+import sparkingOpening from './sparking-opening.mp4';
+import gtOpening from './gt-opening.mp4';
+import majin from './majin.mp4';
+import gt from './gt.mp4';
+import frieza from './frieza.mp4';
+
+export {
+  frieza,
+  legendsOpening,
+  kakarotOpening,
+  sparkingOpening,
+  gtOpening,
+  majin,
+  gt
+};
