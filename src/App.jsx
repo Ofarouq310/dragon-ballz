3298da3a4703d0ef30ddd880c2e1e1f098c900c5
Ofarouq310 <ofarouq310@gmail.com>
2025-08-09 00:40:20 +0300

    Import Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
 import Hero from "./sections/Hero"
 
 function App() {
 
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Header />
       <Hero />
     </main>
   )
