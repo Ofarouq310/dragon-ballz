901c3ab0b4bc39831331ca1cde5016d24c66f2fa
Ofarouq310 <ofarouq310@gmail.com>
2025-08-23 21:38:41 +0300

    Update Button component styles and image size for consistency

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,9 @@ import clsx from "clsx"
 
 export default function Button({title, image, containerClass}) {
   return (
-    <button className={clsx("flex items-center gap-1 px-5 py-2 font-uppercase font-robert-medium text-md group relative text-black w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50", containerClass)} 
+    <button className={clsx("flex items-center gap-1 px-5 py-2 font-uppercase font-robert-medium text-md group relative w-fit cursor-pointer overflow-hidden rounded-full", containerClass)} 
     >
-      <img src={image} alt="One Star Dragon Ball" className="w-6" />
+      <img src={image} alt="One Star Dragon Ball" className="w-4" />
       {title}
     </button>
   )
