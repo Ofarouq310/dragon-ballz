c021a037751d749a79c1ef1272a73f4a7094fd2f
Ofarouq310 <ofarouq310@gmail.com>
2025-08-23 21:41:19 +0300

    Update Features component to hold different game cards

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,8 +1,11 @@
-import {frieza} from "../assets/videos/index"
+import GameCard from "../components/GameCard.jsx";
+
+import { kakarot, breakers, sparkingZero, superHeroes, xenoverse, fighterZ } from "../assets/videos/index"
 
 export default function Features() {
+
   return (
-    <section className='min-h-screen bg-black pb-[200px]' id='features'>
+    <section className='min-h-dvh bg-black pb-[200px]' id='features'>
       <div className="container mx-auto md:px-10 px-3">
 
         <div className="py-32 px-5 font-circularweb text-blue-50 text-lg">
@@ -14,30 +17,19 @@ export default function Features() {
         </div>
 
         <div>
-          <div className="grid grid-rows-4 grid-cols-2 gap-10 grid-flow-row justify-items-center">
-            <div className="relative w-full h-80 border-1 border-amber-100 col-span-2 flex items-end justify-end">
-              <div className="absolute size-full z-10 top-0 left-0 flex flex-col text-blue-50 p-5">
-                <h4 className="card-title special-font">
-                  Fri<b>e</b>za
-                </h4>
-                <p className="max-w-64 mt-3 md:text-base text-xs">
-                  Frieza is .....
-                </p>
-              </div>
-
-              <div className="bg-gray-900 h-full clip-parallelogram">
-                <video src={frieza} className="size-full origin-center object-cover opacity-30" autoPlay muted playsInline loop />    
-              </div>
+          <div className="relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+            <GameCard video={kakarot} title={<><b>K</b>ak<b>a</b>rot</>} description={'A sprawling action RPG that allows players to relive the iconic Dragon Ball Z story, battling familiar villains and exploring the world'} />
+          </div>
+          <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+            <div className="md:col-span-1 md:row-span-2 col-span-2 row-span-1">
+              <GameCard video={sparkingZero} title={<>Sp<b>a</b>r<b>k</b>ing <b>Ze</b>ro</>} description={'Sparking Zero is a 3D team fighting game and expansion onto earlier games such as Budokai Tenkaichi 3, Budokai Tenkaichi 2, and Budokai Tenkaichi'} />
             </div>
-            <div className="w-full h-full border-1 border-amber-100 row-span-2" />
-            <div className="w-full h-80 border-1 border-amber-100" />
-            <div className="w-full h-80 border-1 border-amber-100" />
-            <div className="w-full h-80 border-1 border-amber-100" />
-            <div className="w-full h-80 border-1 border-amber-100" />
+            <GameCard video={xenoverse} title={<><b>X</b>en<b>o</b>verse</>} description={'History is being attacked by evil intruders, players will have to protect the Dragon Ball world'} />
+            <GameCard video={fighterZ} title={<>Fig<b>ht</b>er <b>Z</b></>} description={'Involves the player picking a team of three characters and a unique assist for each, then fighting an AI'} />
+            <GameCard video={breakers} title={<>Bre<b>ak</b>ers</>} description={'An Action game in which players take on the role of the "Raider" who hunts down the rest of players'} />
+            <GameCard video={superHeroes} title={<>Su<b>p</b>er <b>H</b>ero<b>e</b>s</>} description={'Uses a turn-based card battle system in five rounds or less'} />
           </div>
         </div>
-
-
       </div>
     </section>
   )
