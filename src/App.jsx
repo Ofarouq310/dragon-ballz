8ce5d7429320983d4f8d5adcee4f329db79c54d9
Ofarouq310 <ofarouq310@gmail.com>
2025-08-24 20:46:47 +0300

    Add Contact and Footer components to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./sections/About"
 import Features from "./sections/Features"
 import Story from "./sections/Story"
 import Glance from "./sections/Glance"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
       <Features />
       <Story />
       <Glance />
+      <Contact />
+      <Footer />
     </main>
   )
 }
